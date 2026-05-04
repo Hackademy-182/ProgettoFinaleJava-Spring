64f645eee1f5d6ccddba1f7545b7452109a086b3
Tommaso Mancuso <tjmancuso@virgilio.it>
2026-05-04 18:38:49 +0200

    non riesce login su browser

--- src/main/java/aulab/it/the_aulab_chronicle/controllers/UserController.java
+++ src/main/java/aulab/it/the_aulab_chronicle/controllers/UserController.java
@@ -2,6 +2,7 @@ package aulab.it.the_aulab_chronicle.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import aulab.it.the_aulab_chronicle.services.UserService;
 
@@ -10,4 +11,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
 }
